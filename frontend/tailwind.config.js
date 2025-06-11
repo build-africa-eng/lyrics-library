1b8b7a326e6db2b08a1b3fed9710abb04322d8e5
build-africa-eng <afrcanfuture@gmail.com>
2025-06-11 21:28:31 +0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import { zinc } from 'tailwindcss/colors'
 export default {
   content: [
     './index.html',
@@ -8,7 +7,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        zinc,
         primary: '#1e40af',
         secondary: '#60a5fa',
       },

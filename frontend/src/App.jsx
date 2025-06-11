043602abbfcba952177f255a9398fd3be4b1cbaa
build-africa-eng <afrcanfuture@gmail.com>
2025-06-11 22:43:59 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
-import About from "@/pages/About";
-import Library from "@/pages/Library";
+// src/App.jsx
+import { LyricsProvider } from './context/LyricsContext';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '@/pages/Home';
+import About from '@/pages/About';
+import Library from '@/pages/Library';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/library" element={<Library />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <LyricsProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </LyricsProvider>
   );
 }

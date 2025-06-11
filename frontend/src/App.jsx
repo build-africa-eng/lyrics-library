9b253f236bcbd8538e5e1d130f9b583f28fcd52f
build-africa-eng <afrcanfuture@gmail.com>
2025-06-11 14:13:18 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Library from "@/pages/Library";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/library" element={<Library />} />
       <Route path="/about" element={<About />} />
     </Routes>
-  )
+  );
 }

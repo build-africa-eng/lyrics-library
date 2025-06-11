7de80e00dcf3fe1626ba8bdeba6acc2e6a959d3c
build-africa-eng <afrcanfuture@gmail.com>
2025-06-11 12:07:02 +0300

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,17 +1,9 @@
-import { getLyrics } from "./routes/getlyrics";
-import { addLyrics } from "./routes/addlyrics";
-import { scrapeLyrics } from "./routes/scrapelyrics";
-import { getDB } from "./db";
+import { router } from './routes/router.js';
+import { getDB } from './db.js';
 
 export default {
-  async fetch(req, env) {
-    const url = new URL(req.url);
+  async fetch(req, env, ctx) {
     const db = getDB(env);
-
-    if (req.method === "GET" && url.pathname === "/lyrics") return getLyrics(req, db);
-    if (req.method === "POST" && url.pathname === "/lyrics") return addLyrics(req, db);
-    if (req.method === "POST" && url.pathname === "/scrape") return scrapeLyrics(req, db);
-
-    return new Response("Not found", { status: 404 });
+    return router(req, db);
   }
 };

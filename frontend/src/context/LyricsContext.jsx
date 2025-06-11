ce5d56abde687ce1ea515d629eb81aa49174042a
build-africa-eng <afrcanfuture@gmail.com>
2025-06-11 17:33:07 +0300

    Update LyricsContext.jsx

--- frontend/src/context/LyricsContext.jsx
+++ frontend/src/context/LyricsContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from "react";
-import * as api from "@/utils/api";
+limport { createContext, useContext, useState } from 'react';
+import * as api from '@/utils/api';
 
 const LyricsContext = createContext();
 

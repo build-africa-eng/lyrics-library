f2ae81c120dcf21f45b55ab65f513d3369911939
build-africa-eng <afrcanfuture@gmail.com>
2025-06-11 13:23:34 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-lexport default function App() {
+export default function App() {
   return (
     <div className="min-h-screen grid place-items-center">
       <h1 className="text-3xl font-bold tracking-tight">Lyrics Library</h1>

e64cfba7866953f4108617981d69710b18397e2f
build-africa-eng <afrcanfuture@gmail.com>
2025-06-23 22:56:48 +0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       colors: {
         primary: '#1e40af',
         secondary: '#60a5fa',
+        zinc: {
+          100: '#f4f4f5',
+          400: '#a1a1aa',
+          600: '#52525b',
+          800: '#27272a',
+        },
       },
       fontFamily: {
         sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'sans-serif'],

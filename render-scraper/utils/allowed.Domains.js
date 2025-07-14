8c466a6c53a888a10ac636aca8caa8bf3e42153e
build-africa-eng <afrcanfuture@gmail.com>
2025-07-14 18:29:43 +0300

    Create allowed.Domains.js

---
+++ render-scraper/utils/allowed.Domains.js
@@ -0,0 +1,24 @@
+// utils/allowedDomains.js
+
+const ALLOWED_DOMAINS = [
+  'genius.com',
+  'www.genius.com',
+  'azlyrics.com',
+  'www.azlyrics.com',
+  'lyrics.com',
+  'www.lyrics.com'
+];
+
+/**
+ * Checks if a URL belongs to an allowed lyrics domain
+ * @param {string} url - Full URL to check
+ * @returns {boolean}
+ */
+export function isAllowedDomain(url) {
+  try {
+    const { hostname } = new URL(url);
+    return ALLOWED_DOMAINS.includes(hostname.toLowerCase());
+  } catch {
+    return false;
+  }
+}
